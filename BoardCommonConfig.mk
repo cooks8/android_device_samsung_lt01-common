4af45bc317cb242e1bff20c636785e80b46d66f5
Dave Martin <davemartinfsu@gmail.com>
2016-03-14 14:57:30 -0400

    Blissify

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -20,6 +20,21 @@
 
 TARGET_SPECIFIC_HEADER_PATH := device/samsung/lt01-common/include
 
+# BlissPop Config Flags
+TARGET_TC_ROM := 5.3-sm
+TARGET_TC_KERNEL := 6.0-linaro
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_STRICT := false
+BLISS_GRAPHITE := false
+BLISS_KRAIT := false
+BLISS_PIPE := true
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+WITH_LZMA_OTA := true
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
+
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 
